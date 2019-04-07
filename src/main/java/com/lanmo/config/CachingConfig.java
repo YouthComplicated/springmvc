60ded1d02e59c381422df72dcfd2c4dd3fad58e7
nyq86369196@git <2041699001@qq.com>
2019-04-07 16:15:31 +0800

    *)spring jdbc 4-7

--- src/main/java/com/lanmo/config/CachingConfig.java
+++ src/main/java/com/lanmo/config/CachingConfig.java
@@ -6,9 +6,17 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableCaching
 public class CachingConfig {
@@ -31,18 +39,56 @@ public class CachingConfig {
 
 
     /**
-     * 使用redis缓存
+     * 使用redis缓存管理器bean
      * @return
      */
     @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate){
+    public org.springframework.cache.CacheManager cacheManager(RedisTemplate redisTemplate){
+        /**
+         * 没有该构造器??? 版本的问题?
+         */
+//        return new RedisCacheManager(redisTemplate);
         return null;
     }
+
+    /**
+     * redis 连接工厂bean
+     * @return
+     */
     @Bean
-    public EhCacheManagerFactoryBean cache(){
-        EhCacheManagerFactoryBean ehCaheManagerFactoryBean = new EhCacheManagerFactoryBean();
-        ehCaheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
-        return ehCaheManagerFactoryBean;
+    public JedisConnectionFactory redisConnectionFactory(){
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.afterPropertiesSet();
+        return jedisConnectionFactory;
+    }
+
+    /**
+     * redisTemplate bean
+     * @param redisCF
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisCF){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisCF);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    /**
+     * 配置多个缓存管理器
+     *
+     * todo redis  如何创建redisManager未解决
+     * @param cm
+     * @return
+     */
+    @Bean
+    public org.springframework.cache.CacheManager cacheManager(net.sf.ehcache.CacheManager cm){
+        CompositeCacheManager cacheManager = new CompositeCacheManager();
+        List<org.springframework.cache.CacheManager> managers = new ArrayList<>();
+        managers.add(new EhCacheCacheManager(cm));
+//        managers.add(new RedisCacheManager(redisTemplate()));
+        return cacheManager;
     }
 
 
